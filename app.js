f32d15ce18def37f16e50f37b77bb69ac8689f3a
stockology <124348998+stockology@users.noreply.github.com>
2023-02-03 15:29:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ import course from "./Routes/courseRoutes.js";
 import user from "./Routes/userRoutes.js";
 import payment from "./Routes/paymentRoutes.js";
 import other from "./Routes/otherRoutes.js";
-app.use("api/v1", course);
-app.use("api/v1", user);
-app.use("api/v1", payment);
-app.use("api/v1", other);
+app.use("/api/v1", course);
+app.use("/api/v1", user);
+app.use("/api/v1", payment);
+app.use("/api/v1", other);
 
 app.get("/", (req, res) =>
   res.send(
